62e262548db89c053dc7d7b43349628af3fbd3e4
markclaybeep <mark.clay@willowfield-school.co.uk>
2025-06-02 12:01:30 +0100

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,9 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showIcon(IconNames.Surprised)
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showIcon(IconNames.Happy)
+})
+basic.forever(function () {
+	
+})
